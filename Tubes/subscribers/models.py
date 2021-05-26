6e6e5e6233fa93d352c41f940c654359eb559f96
HafizhL15 <83531538+HafizhL15@users.noreply.github.com>
2021-05-26 09:44:58 +0700

    Update models.py

--- Tubes/subscribers/models.py
+++ Tubes/subscribers/models.py
@@ -3,11 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Pelanggan(models.Model):
-	tipe	  	= models.CharField(max_length=100)
-	name 		= models.CharField(max_length=100)
-	address		= models.CharField(max_length=100)
-	phone		= models.CharField(max_length=100)
-	email		= models.EmailField(max_length=100)
+	id 				= models.IntegerField()
+	subscriber_id 	= models.IntegerField(max_length=7,primary_key=True)
+	tipe		  	= models.CharField(max_length=100)
+	name 			= models.CharField(max_length=100)
+	address			= models.CharField(max_length=100)
+	phone			= models.CharField(max_length=100)
+	email			= models.EmailField(max_length=100)
 
 	def __str__(self):
-		return "{}.{}".format(self.id,self.name)
+		return "{}".format(self.subscriber_id)
